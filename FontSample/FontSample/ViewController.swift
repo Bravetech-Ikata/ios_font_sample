a2560e26949d065744c9de4662b068ecdca45ff3
ikata <keisuke.ikata@bravetechnology.co.jp>
2016-03-22 10:08:15 +0900

    ファミリー名とフォント名を切り替えられるように対応

--- FontSample/FontSample/ViewController.swift
+++ FontSample/FontSample/ViewController.swift
@@ -10,23 +10,31 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    enum FONT_TYPE: Int {
+        case FAMILY
+        case FONT
+    }
+    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var btnChange: UIButton!
     
     var familyNameArr: [String] = [String]()
-    var fontNamrArr: [String] = [String]()
+    var fontNameArr: [String] = [String]()
+    var currentType: FONT_TYPE = .FAMILY
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.familyNameArr = UIFont.familyNames()
         for item in self.familyNameArr {
-            for item2 in UIFont.fontNamesForFamilyName(item) {
-                self.fontNamrArr.append(item2)
-            }
+            self.fontNameArr.append(item)
         }
+        
+        self.btnChange.setTitle("フォントに変更", forState: UIControlState.Normal)
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,13 +51,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK - UITableViewDelegate
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.fontNamrArr.count
+        return self.fontNameArr.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: FontCell = tableView.dequeueReusableCellWithIdentifier("FontCell", forIndexPath: indexPath) as! FontCell
         
-        cell.setLabelFont(self.fontNamrArr[indexPath.row])
+        cell.setLabelFont(self.fontNameArr[indexPath.row])
         
         return cell
     }
@@ -58,8 +66,39 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.deselectRowAtIndexPath(indexPath, animated: false)
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         let vc: DetailViewController = storyBoard.instantiateViewControllerWithIdentifier("DetailViewController") as! DetailViewController
-        vc.fontName = self.fontNamrArr[indexPath.row]
+        vc.fontName = self.fontNameArr[indexPath.row]
         self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func pushBtnChange(sender: AnyObject) {
+        self.changeFontType()
+    }
+    
+    
+    private func changeFontType() {
+        switch self.currentType {
+        case .FONT:
+            self.btnChange.setTitle("フォントに変更", forState: UIControlState.Normal)
+            self.currentType = .FAMILY
+            self.familyNameArr = UIFont.familyNames()
+            self.fontNameArr.removeAll()
+            for item in self.familyNameArr {
+                self.fontNameArr.append(item)
+            }
+            break
+        case .FAMILY:
+            self.btnChange.setTitle("ファミリーに変更", forState: UIControlState.Normal)
+            self.currentType = .FONT
+            self.familyNameArr = UIFont.familyNames()
+            self.fontNameArr.removeAll()
+            for item in self.familyNameArr {
+                for item2 in UIFont.fontNamesForFamilyName(item) {
+                    self.fontNameArr.append(item2)
+                }
+            }
+            break
+        }
+        self.tableView.reloadData()
     }
 }
 
